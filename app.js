589f92a91b22631e7c6d427d67ef185a4ce6977e
Ken Chen <chenchaokun@gmail.com>
2015-07-28 13:46:41 +0800

    initial commit

---
+++ app.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser')
+var ueditor = require("ueditor")
+var app = express();
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
+app.use(express.static(__dirname + '/public'));
+
+app.use("/ueditor/ue", ueditor(path.join(__dirname, 'public'), function(req, res, next) {
+  // ueditor 客户发起上传图片请求
+  if(req.query.action === 'uploadimage'){
+    var foo = req.ueditor;
+    var date = new Date();
+    var imgname = req.ueditor.filename;
+
+    var img_url = '/images/';
+    res.ue_up(img_url); //你只要输入要保存的地址 。保存操作交给ueditor来做
+  }
+  //  客户端发起图片列表请求
+  else if (req.query.action === 'listimage'){
+    var dir_url = '/images';
+    res.ue_list(dir_url);  // 客户端会列出 dir_url 目录下的所有图片
+    console.log('listimage');
+  }
+  // config
+  else if (req.query.action === 'config') {
+    res.setHeader('Content-Type', 'application/json');
+    res.redirect('/ueditor/ueditor.config.json')
+  }
+  // 客户端发起其它请求
+  else {
+    console.log(req.url);
+}}));
+
+app.listen(3000, function () {
+    console.log('app listening at port 3000');
+});
